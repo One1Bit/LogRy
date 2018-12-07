098089b9b8201f14ab1dff2157ccf0af0f35ecdf
One1Bit <sorus.rose@gmail.ru>
2018-12-07 22:30:23 +0300

    First Ver

---
+++ LogRy/Setting.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Windows.Automation;
+using System.Windows.Controls.Primitives;
+using System.Windows.Ink;
+using System.Windows.Markup;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Media3D;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shell;
+namespace LogRy
+{
+    /// <summary>
+    /// Логика взаимодействия для SettingColumns.xaml
+    /// </summary>
+    public partial class Setting : Window
+    {
+        
+
+        public Setting()
+        {
+            InitializeComponent();
+        }
+    
+    }
+}
