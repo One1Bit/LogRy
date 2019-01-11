00f21f289c6c8d4a400d44be59d2a84f011084e5
One1Bit <One1Bit@bk.ru>
2019-01-11 21:38:23 +0300

    Count and Name Col

--- LogRy/MainWindow.xaml.cs
+++ LogRy/MainWindow.xaml.cs
@@ -48,12 +48,12 @@ namespace LogRy
         {
             string Split = DataResult.ResultSplitSetting;
             if (Split == null)
-            {
-                MessageBox.Show("Error! Split = ;");
                 Split = ";";
-            }
+            int i= DataResult.ResultCountColumns;
+            if (i == 0)
+                i = 4;
             var fileDialog = new System.Windows.Forms.OpenFileDialog();
-            List<DataTable> list = new List<DataTable>();
+            List<DataTabRes> list = new List<DataTabRes>();
             if (fileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 try
@@ -63,8 +63,9 @@ namespace LogRy
                         string line;
                         while ((line = sr.ReadLine()) != null)
                         {
-                            var parsed = line.Split(Convert.ToChar(Split)); //Делим строку по символу ;
-                            list.Add(new DataTable(parsed[0], parsed[1], parsed[2], parsed[3]));
+                            string[] parsed = new string[i];
+                            parsed = line.Split(Convert.ToChar(Split)); //Делим строку по символу
+                            list.Add(new DataTabRes(parsed, i));
                         }
                     }
                     this.Title = $"LogRy {fileDialog.FileName} ";
@@ -75,6 +76,16 @@ namespace LogRy
                     MessageBox.Show($"{a}");
                 }
                 DataGrid.ItemsSource = list;
+                DataGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
+                DataGrid.VerticalAlignment = VerticalAlignment.Stretch;
+
+                for (int o = i; o < 7; o++)
+                    DataGrid.Columns[o].Visibility = Visibility.Collapsed;
+                if (DataResult.ResultColumns != null)
+                {
+                    for (int u = 0; u < i; u++)
+                        DataGrid.Columns[u].Header = DataResult.ResultColumns[u];
+                }
             }
 
         }
@@ -94,7 +105,7 @@ namespace LogRy
                         string line;
                         while ((line = sr.ReadLine()) != null)
                         {
-                            var parsed = line.Split(';'); //Делим строку по символу &, например
+                            var parsed = line.Split(';'); //Делим строку по символу 
                             list.Add(new DataTable(parsed[0], parsed[1], parsed[2], parsed[3]));
                         }
                     }
@@ -114,7 +125,12 @@ namespace LogRy
 
         }
 
-        private void ClearClick(object sender, RoutedEventArgs e) =>  DataGrid.ItemsSource = null;
+        private void ClearClick(object sender, RoutedEventArgs e)
+        {
+            DataGrid.ItemsSource = null;
+            this.Title = "LogRy";
+        }
+
 
         private void SettingClick(object sender, RoutedEventArgs e)
         {
@@ -142,8 +158,60 @@ namespace LogRy
         public string Message { get; set; }
 
     }
+    class DataTabRes
+    {
+        public DataTabRes(string[] par, int i)
+        {
+            for (int j = 0; j < i; j++)
+            {
+
+                switch (j)
+                {
+                    case 0:
+                        this.Col1 = par[j];
+                        break;
+                    case 1:
+                        this.Col2 = par[j];
+                        break;
+                    case 2:
+                        this.Col3 = par[j];
+                        break;
+                    case 3:
+                        this.Col4 = par[j];
+                        break;
+                    case 4:
+                        this.Col5 = par[j];
+                        break;
+                    case 5:
+                        this.Col6 = par[j];
+                        break;
+                    case 6:
+                        this.Col7 = par[j];
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }
+
+        }
+        public string Col1 { get; set; }
+        public string Col2 { get; set; }
+        public string Col3 { get; set; }
+        public string Col4 { get; set; }
+        public string Col5 { get; set; }
+        public string Col6 { get; set; }
+        public string Col7 { get; set; }
+
+
+    }
     class DataResult
     {
         public static string ResultSplitSetting { get; set; }
+        public static int ResultCountColumns { get; set; }
+        public static string[] ResultColumns { get; set; }
+        public static string[] ResultColumnsTypeData { get; set; }
+
     }
 }
--- LogRy/Setting.xaml.cs
+++ LogRy/Setting.xaml.cs
@@ -19,7 +19,7 @@ namespace LogRy
 
 
 
-        private string RadioButtonChecked(object sender, RoutedEventArgs e)
+        private string RadioButtonSplitChecked(object sender, RoutedEventArgs e)
         {
             
             if (tab.IsChecked == true)
@@ -34,15 +34,28 @@ namespace LogRy
             MessageBox.Show("Error in Setting Split!");
             return null;
         }
-       
+       private void ComboBoxColumsChecked(object sender, RoutedEventArgs e)
+        {
+            DataResult.ResultColumns = new string[7];
+
+                DataResult.ResultColumns[0] = Convert.ToString(TB1.Text);
+                DataResult.ResultColumns[1] = Convert.ToString(TB2.Text);
+                DataResult.ResultColumns[2] = Convert.ToString(TB3.Text);
+                DataResult.ResultColumns[3] = Convert.ToString(TB4.Text);
+                DataResult.ResultColumns[4] = Convert.ToString(TB5.Text);
+                DataResult.ResultColumns[5] = Convert.ToString(TB6.Text);
+                DataResult.ResultColumns[6] = Convert.ToString(TB7.Text);
+        }
+      
+        int ResultCountColumns;
+
 
         private void ComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            int i=0;
+            int i = 0;
             if (StackPanelColName != null)
                 StackPanelColName.Children.Clear();
-            if (StackPanelDataType != null)
-                StackPanelDataType.Children.Clear();
+            
             string localVar="4";
             ComboBoxItem selectedItem = (ComboBoxItem)ComboBox.SelectedItem;
             if (selectedItem.Content != null)
@@ -50,26 +63,23 @@ namespace LogRy
                 localVar = selectedItem.Content.ToString();
                 i = Convert.ToInt32(localVar);
                 TextBox[] TextBoxMass = new TextBox[i];
-                ComboBox[] ComboBoxMass = new ComboBox[i];
+                ResultCountColumns = i;
+
 
                 for (int j = 0; j < i; j++)
                 {
-                    TextBox TextBoxN = new TextBox();
-                    TextBoxN.Name = $"TextBoxName{j+1}";
-                    TextBoxN.Text = "";
-                    TextBoxN.Height = 25;
-                    StackPanelColName.Children.Add(TextBoxN);
-                    //StackPanelColName.RegisterName(TextBoxN.Name, TextBoxN);
-                    ComboBox ComboBoxN = new ComboBox();
-                    ComboBoxN.Height = 25;
-                    ComboBoxN.Name = $"DataTypeCombpBox{j+1}";
-                    ComboBoxN.Items.Add("DateTime");
-                    ComboBoxN.Items.Add("String");
-                    ComboBoxN.Items.Add("Int");
-                    StackPanelDataType.Children.Add(ComboBoxN);
-                    TextBoxMass[j] = TextBoxN;
-                    ComboBoxMass[j] = ComboBoxN;
+                    TextBoxMass[j] = new TextBox
+                    {
+                        Name = "fr",
+                        Text = "",
+                        Height = 25
+                    };
+
+                    StackPanelColName.Children.Add(TextBoxMass[j]);
+                    
                 }
+
+
             }
           
         }
@@ -82,8 +92,19 @@ namespace LogRy
         private void OkClick(object sender, RoutedEventArgs e)
         {
             string result;
-            result = RadioButtonChecked(sender, e);
+            result = RadioButtonSplitChecked(sender, e);
+            if (result == null)
+            {
+                result = ";";
+            }
             DataResult.ResultSplitSetting = Convert.ToString(result);
+            if (ResultCountColumns == 0)
+                DataResult.ResultCountColumns = 4;
+            else
+            DataResult.ResultCountColumns = ResultCountColumns;
+           
+
+            ComboBoxColumsChecked(sender, e);
             this.Close();
         }
     }
